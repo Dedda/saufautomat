f6b27c5e5909f2b23d74a962d7e6ea9cb9ff3a8f
Stephan Goeppentin <dedda102@gmail.com>
2020-01-23 21:56:04 +0100

    Conserve power
    Save some power by disabling unused ports and hardware features

--- Saufautomat.ino
+++ Saufautomat.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal.h>
 #include <SPI.h>
 #include <SD.h>
+#include <avr/power.h>
 
 LiquidCrystal lcd(7, 6, 2, 3, 4, 5);
 
@@ -23,11 +24,24 @@ byte non_alcoholics = 0;
 byte longdrinks = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+
+  power_adc_disable();
+  power_usart0_disable();
+  power_usart2_disable();
+  power_timer1_disable();
+  power_timer2_disable();
+  power_timer3_disable();
+  power_timer4_disable();
+  power_timer5_disable();
+  power_twi_disable();
+
+  // turn off on board LED to save some power
+  turnOffDigitalPins();
+  turnOffAnalogPins();
+
   pinMode(9, OUTPUT);
   busy();
   pinMode(resetPin, INPUT_PULLUP);
-  Serial.begin(9600);
   lcd.begin(16, 2);
   bootAnimation();
   initSD();
@@ -165,9 +179,7 @@ byte loadBeverage(File f) {
 }
 
 void initSD() {
-  if (!SD.begin(53)) {
-    Serial.println("Error initializing sd card!");
-  }
+  SD.begin(53);
 }
 
 void checkReset() {
@@ -201,5 +213,47 @@ void checkReset() {
         break;
       }
     }
+  }
+}
+
+void turnOffAnalogPins() {
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+  pinMode(A6, OUTPUT);
+  pinMode(A7, OUTPUT);
+  pinMode(A8, OUTPUT);
+  pinMode(A9, OUTPUT);
+  pinMode(A10, OUTPUT);
+  pinMode(A11, OUTPUT);
+  pinMode(A12, OUTPUT);
+  pinMode(A13, OUTPUT);
+  pinMode(A14, OUTPUT);
+  pinMode(A15, OUTPUT);
+  digitalWrite(A0, LOW);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
+  digitalWrite(A4, LOW);
+  digitalWrite(A5, LOW);
+  digitalWrite(A6, LOW);
+  digitalWrite(A7, LOW);
+  digitalWrite(A8, LOW);
+  digitalWrite(A9, LOW);
+  digitalWrite(A10, LOW);
+  digitalWrite(A11, LOW);
+  digitalWrite(A12, LOW);
+  digitalWrite(A13, LOW);
+  digitalWrite(A14, LOW);
+  digitalWrite(A15, LOW);
+}
+
+void turnOffDigitalPins() {
+  for (int i = 0; i < 54; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
   }
 }
